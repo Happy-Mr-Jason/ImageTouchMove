4644e4ac55f8276f9b3d3f1dab00185ec2f8cdbf
Happy-Mr-Jason <happy.seongjin.project@gmail.com>
2020-01-07 14:23:52 +0900

    Added Function to switch TouchEvent

--- app/src/main/java/com/example/imagetouchmove/MainActivity.java
+++ app/src/main/java/com/example/imagetouchmove/MainActivity.java
@@ -22,8 +22,10 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout layoutMain;
+    Button btnHolder;
     Button btn1;
     ImageView ivBlue, ivRed, ivGreen, ivYellow;
+    private boolean widgetHold;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -32,23 +34,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         layoutMain = findViewById(R.id.layoutMain);
+        btnHolder = findViewById(R.id.btnHolder);
         btn1 = findViewById(R.id.btn1);
         ivBlue = findViewById(R.id.ivBlue);
         ivRed = findViewById(R.id.ivRed);
         ivGreen = findViewById(R.id.ivGreen);
         ivYellow = findViewById(R.id.ivYellow);
 
-        btn1.setOnTouchListener(new MyTouchListener());
-        ivBlue.setOnTouchListener(new MyTouchListener());
-        ivRed.setOnTouchListener(new MyTouchListener());
-        ivGreen.setOnTouchListener(new MyTouchListener());
-        ivYellow.setOnTouchListener(new MyTouchListener());
-
+        btnHolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(widgetHold == false) {
+                    btn1.setOnTouchListener(null);
+                    ivBlue.setOnTouchListener(null);
+                    ivRed.setOnTouchListener(null);
+                    ivGreen.setOnTouchListener(null);
+                    ivYellow.setOnTouchListener(null);
+                    btnHolder.setText("고정해제");
+                    widgetHold = true;
+                }else {
+                    btn1.setOnTouchListener(new MyTouchListener());
+                    ivBlue.setOnTouchListener(new MyTouchListener());
+                    ivRed.setOnTouchListener(new MyTouchListener());
+                    ivGreen.setOnTouchListener(new MyTouchListener());
+                    ivYellow.setOnTouchListener(new MyTouchListener());
+                    btnHolder.setText("고정");
+                    widgetHold = false;
+                }
+            }
+        });
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "버튼 눌렸다", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        btnHolder.callOnClick();
     }
 }
